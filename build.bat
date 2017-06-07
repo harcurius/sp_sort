3270de8b733a9dce8d1049db860f0d67d9ba3006
sander.persen@hig.no <sander.persen@hig.no>
2017-06-07 11:31:45 +0200

    edited build bat file

--- build.bat
+++ build.bat
@@ -1,36 +1,20 @@
 @echo off
-setlocal enabledelayedexpansion
+
+set VCVarsAllPath="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+set SourceFile="a:/sp_sort/code/win32.c"
+set BuildPath="A:\sp_sort\build"
 
 ctime -begin "sp_sort.ctm"
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-
-set ExeName="sp_sort"
-set SourceFile="a:/sp_sort/code/win32.c"
-
-set Includes="A:\projectmanager\Library"
-
-set BuildSpeed="F"
-set BuildMode="D"
-set BuildDefine=""
+call %VCVarsAllPath% x64
 
 REM Replace -MD with -MT for less dependencies (IE no VCRuntimeXXX.dll missing)
 REM -MD Is faster for build than -MT
 
-set CommonCompilerFlags=-Zi -MD -nologo -Gm -EHa -wd4311 -wd4312 /I%Includes%
+set CommonCompilerFlags=-Zi -MD -nologo -Gm -EHa -wd4311 -wd4312
 set CommonLinkerFlags=/incremental:no /opt:ref User32.lib Gdi32.lib OpenGL32.lib
 
-if %BuildMode% == "D" (
-	set CommonCompilerFlags=%CommonCompilerFlags% -Od
-	set BuildDefine=DEBUG_BUILD
-) else if %BuildMode% == "R" (
-	set CommonCompilerFlags=%CommonCompilerFlags% -O2
-	set BuildDefine=RELEASE_BUILD
-)
-
-set BuildPath="A:\sp_sort\build"
-
 pushd %BuildPath%
-cl %CommonCompilerFlags% -D%BuildDefine% -Fe%ExeName% %SourceFile% /link %CommonLinkerFlags%
+cl %CommonCompilerFlags% %SourceFile% /link %CommonLinkerFlags%
 set LastError=%ERRORLEVEL%
 popd
 ctime -end "sp_sort.ctm" %ERRORLEVEL%
